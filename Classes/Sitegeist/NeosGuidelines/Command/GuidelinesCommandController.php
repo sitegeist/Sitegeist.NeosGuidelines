7c8db0b2229d33863cf4fcee4eea1ab99e6e924c
Max Strübing <struebing@sitegeist.de>
2016-12-12 14:18:06 +0100

    TASK: Remove npm-shrinkwrap

--- Classes/Sitegeist/NeosGuidelines/Command/GuidelinesCommandController.php
+++ Classes/Sitegeist/NeosGuidelines/Command/GuidelinesCommandController.php
@@ -20,7 +20,6 @@ class GuidelinesCommandController extends CommandController
      */
     const EDITORCONFIG = '.editorconfig';
     const COMPOSER_LOCK = 'composer.lock';
-    const NPM_SHRINKWRAP = 'npm-shrinkwrap.json';
     const README = 'README.md';
 
     /**
@@ -54,7 +53,6 @@ class GuidelinesCommandController extends CommandController
         $files = array(
             self::EDITORCONFIG,
             self::COMPOSER_LOCK,
-            self::NPM_SHRINKWRAP,
             self::README
         );
 
